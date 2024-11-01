85015d242325be4f2e807ea6e00eabe7d98165b4
listcodename <angelzero16@hotmail.com>
2024-11-01 13:41:10 -0300

    Logged.html + modificaciones en css y js

--- BD2UNER/js/login.js
+++ BD2UNER/js/login.js
@@ -1,3 +1,28 @@
-document.getElementById("close-login").addEventListener('click', ()=>{
-    document.getElementById("login").style.right = "-350px";
+let modalLogin = document.getElementById("modal-login");
+let registro = document.getElementById("register");
+let login = document.getElementById("login");
+let btnCerrarLogin = document.getElementById("close-login");
+let btnSwitchLogin = document.getElementById("switch");
+let btnAbrirLogin = document.getElementById("open-login");;
+
+let flagLogReg = true;
+
+btnCerrarLogin.addEventListener('click', ()=>{
+    modalLogin.style.right = "-400px";
+});
+
+btnAbrirLogin.addEventListener('click', ()=>{
+    modalLogin.style.right = "0px";
+});
+
+btnSwitchLogin.addEventListener('click', ()=>{
+    if(flagLogReg){
+        login.style.display = "none";
+        registro.style.display = "flex";
+        flagLogReg = false;
+    }else{
+        login.style.display = "flex";
+        registro.style.display = "none";
+        flagLogReg = true;
+    }
 });
